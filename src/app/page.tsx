2948be5c2268ae512f53037015b58a91d3df2156
manoj <jmanojjmvr49@gmail.com>
2025-04-19 00:31:42 +0530

    Add Features component to showcase powerful tools for project management

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/Hero";
 import { Navbar } from "@/components/mainNavbar";
+import Features from "@/components/Features"
 export default function Home() {
   return (
     <div className="container mx-auto w-full">
       <Navbar />
        <Hero />
+       <Features />
     </div>
   );
 }
