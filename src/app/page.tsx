9a689db7b597473cfa1d5003778c01e7af1dabee
manoj <jmanojjmvr49@gmail.com>
2025-04-19 01:53:22 +0530

    Remove Testimonials component and its usage from the Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import Hero from "@/components/Hero";
 import { Navbar } from "@/components/mainNavbar";
 import Features from "@/components/Features"
 import Pricing from "@/components/Pricing";
-import Testimonials from "@/components/Testimonials";
 export default function Home() {
   return (
     <div className="container mx-auto w-full">
@@ -10,7 +9,7 @@ export default function Home() {
        <Hero />
        <Features />
        <Pricing />
-       <Testimonials />
+       
     </div>
   );
 }
