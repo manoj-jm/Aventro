121a80c0515585819909f605f88d8c3c199b0844
prathyarthi <prathyarti@gmail.com>
2025-04-20 03:12:31 +0530

    Fix bugs

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-// export default nextConfig;
-
-
-module.exports = {
-    webpack: (config, { isServer }) => {
-        if (isServer) {
-            config.externals.push("prisma");
-        }
-        return config;
-    },
-};
+export default nextConfig;
